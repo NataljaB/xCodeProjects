ff540968b5a38bf1898c67d64f23a06e81cfeafc
NataljaB <natalja.buljko@gmail.com>
2020-08-22 14:47:39 +0300

    first commit

---
+++ UIElementsLayout/UIElementsLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIElementsLayout
+//
+//  Created by natalja.buljko on 22/08/2020.
+//  Copyright © 2020 natalja.buljko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
