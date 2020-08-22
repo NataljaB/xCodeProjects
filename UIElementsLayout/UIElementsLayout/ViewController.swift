affcc17f64f2001d15d3583e6e69477f11580abd
NataljaB <natalja.buljko@gmail.com>
2020-08-22 17:39:10 +0300

    code added

--- UIElementsLayout/UIElementsLayout/ViewController.swift
+++ UIElementsLayout/UIElementsLayout/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftButtonUp: UIButton!
+    @IBOutlet weak var rightButtonUp: UIButton!
+    @IBOutlet weak var leftButtonDown: UIButton!
+    @IBOutlet weak var rightButtonDown: UIButton!
+    
+    
+    @IBOutlet var styleOutletCollection: [UIButton]!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
 
