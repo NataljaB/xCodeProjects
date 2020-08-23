03eb82e7093c42fd384a7f6d4a8c3ea28fa06384
NataljaB <natalja.buljko@gmail.com>
2020-08-23 07:58:34 +0300

    code for buttons, image for one button, drag outside

--- UIElementsLayout/UIElementsLayout/ViewController.swift
+++ UIElementsLayout/UIElementsLayout/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var leftButtonUp: UIButton!
     @IBOutlet weak var rightButtonUp: UIButton!
     @IBOutlet weak var leftButtonDown: UIButton!
@@ -25,9 +24,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        styleUI()
+        
     }
-
-
+        
+    func styleUI(){
+        mainImageView.layer.cornerRadius = 10
+        mainImageView.layer.borderWidth = 2
+        mainImageView.layer.borderColor = UIColor.black.cgColor
+        
+        myLabel.layer.cornerRadius = 10
+        myLabel.layer.borderWidth = 2
+        myLabel.layer.borderColor = UIColor.black.cgColor
+        
+        styleOutletCollection.forEach { button in button.layer.cornerRadius = 10
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.black.cgColor
 }
-
+    }
+    
+    //MARK:- Logic style to myLabel, Buttons and Image
+    
+    func changeButtonImage(with image: String){
+        leftButtonUp.setImage(UIImage(named: "home.png"), for: .normal)
+        leftButtonUp.tintColor = .white
+        leftButtonUp.imageEdgeInsets = UIEdgeInsets(top: 8, left: 0, bottom: 10, right: 15)
+        self.view.backgroundColor = UIColor.black
+    }
+    func changeTextLabel(){
+        
+    }
+    
+    //MARK:- IBAction for three more buttons
+    @IBAction func touchDragOutsideTapped(_ sender: Any) {
+        print("touchDragOutsideTapped")
+        changeButtonImage(with: "home.png")
+    }
+    
+    
+    
+}
